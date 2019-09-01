060cf1a09bbd79e7e44f7cd7f805e2334a4a6b8f
Sanchitgoyal2000 <38776804+Sanchitgoyal2000@users.noreply.github.com>
2019-09-01 16:10:47 +0530

    Add files via upload

---
+++ Amazing Subarrays.cpp
@@ -0,0 +1,27 @@
+int Solution::solve(string A)
+{
+    int p=A.length(),sum=0;
+    for(int i=0;i<A.length();i++)
+    {
+        if(A[i]=='a'||A[i]=='e'||A[i]=='i'||A[i]=='o'||A[i]=='u'||A[i]=='A'||A[i]=='E'||A[i]=='I'||A[i]=='O'||A[i]=='U')
+        sum=sum+p-i;
+    }
+    return sum%10003;
+}
+
+
+/*Input
+    ABEC
+
+Output
+    6
+
+Explanation
+	Amazing substrings of given string are :
+	1. A
+	2. AB
+	3. ABE
+	4. ABEC
+	5. E
+	6. EC
+	here number of substrings are 6 and 6 % 10003 = 6.*/
